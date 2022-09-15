72737f5e24355a75b99661a318a0807826a05fbc
Osijonah <osijonah@gmail.com>
2022-09-15 10:57:23 +0100

    first commmit

---
+++ app.js
@@ -0,0 +1,10 @@
+const inputNumber = document.getElementById("cardnumber");
+const inputName = document.getElementById("cardname");
+const inputMonth = document.getElementById("mm");
+const inputYear = document.getElementById("yy");
+const inputcvc = document.getElementByClassName("cvc-input");
+
+const cardNumber = document.querySelector(".span span");
+const cardName = document.querySelector(".name");
+const cardExp = document.querySelector(".mmyy");
+const cardCVC = document.getElementByClassName("cvc");
